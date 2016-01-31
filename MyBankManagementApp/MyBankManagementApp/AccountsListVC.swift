d60638a5840c30fad3bf40d5968acda42bcc298b
Rigoberto Sáenz Imbacuán <beto456789@gmail.com>
2016-01-31 02:06:01 -0500

    - Added Pods dependency manager - Initial folders structure

---
+++ MyBankManagementApp/MyBankManagementApp/AccountsListVC.swift
@@ -0,0 +1,52 @@
+//
+//  AccountsListVC.swift
+//  MyBankManagementApp
+//
+//  Created by Rigoberto Sáenz Imbacuán on 1/31/16.
+//  Copyright © 2016 Rigoberto Saenz Imbacuan. All rights reserved.
+//
+
+import UIKit
+
+class AccountsListVC: UITableViewController {
+    
+    // -------------------------
+    // UIViewController
+    // -------------------------
+    override func viewDidLoad() { // Called after the view has been loaded. For view controllers created in code, this is after -loadView. For view controllers unarchived from a nib, this is after the view is set.
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) { // Called when the view is about to made visible. Default does nothing
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) { // Called when the view has been fully transitioned onto the screen. Default does nothing
+        
+    }
+    
+    
+    // -------------------------
+    // UITableViewDelegate
+    // -------------------------
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Selected Row: \(indexPath.row)")
+    }
+    
+    
+    // -------------------------
+    // UITableViewDataSource
+    // -------------------------
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell
+        return cell
+    }
+    
+}
